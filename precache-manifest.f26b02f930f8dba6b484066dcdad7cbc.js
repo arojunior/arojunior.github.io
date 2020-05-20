ed8d0f471299f96f8667184614fe3971d40dff04
Antonio De Oliveira <ext.antonio.deolivei@ef.com>
2020-05-20 15:44:57 +0200

    update

---
+++ precache-manifest.f26b02f930f8dba6b484066dcdad7cbc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f868b9e41d646e1aba9dcc853629c6c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81fab5b58843b62690ef",
+    "url": "/static/css/2.14feee9d.chunk.css"
+  },
+  {
+    "revision": "0d4f0283ce55d26fad34",
+    "url": "/static/css/main.9735dd99.chunk.css"
+  },
+  {
+    "revision": "81fab5b58843b62690ef",
+    "url": "/static/js/2.7796c199.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.7796c199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4f0283ce55d26fad34",
+    "url": "/static/js/main.855fa827.chunk.js"
+  },
+  {
+    "revision": "38a99fc5998b32260a92",
+    "url": "/static/js/runtime-main.de8a751f.js"
+  },
+  {
+    "revision": "ef05e42f7cf5f63a807d2273da70932d",
+    "url": "/static/media/banner-bg.ef05e42f.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "36bef19ad606e86d29469a6a0bafccea",
+    "url": "/static/media/intro-bg.36bef19a.jpg"
+  },
+  {
+    "revision": "8cf8d17481f256228223294cfa9190c9",
+    "url": "/static/media/logo-junioroliveira.8cf8d174.png"
+  }
+]);
